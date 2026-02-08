b26880a6f96e4acbf8272892b72e7b3fddb7f631
Your Name <your.email@example.com>
2026-02-08 02:11:33 +0100

    update on api.ts

--- SEEUConnect.Backend/SEEUConnect.Backend/obj/Debug/net10.0/SEEUConnect.Backend.AssemblyInfo.cs
+++ SEEUConnect.Backend/SEEUConnect.Backend/obj/Debug/net10.0/SEEUConnect.Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SEEUConnect.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd95dc442d281fc328e6914cdb7ee8392509ca45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c505d95c0c30601c608b5ab12a83f336467de459")]
 [assembly: System.Reflection.AssemblyProductAttribute("SEEUConnect.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SEEUConnect.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
